fab365169cd4952da03b4d6c7dbbed92e5e2ee5e
cizma <pavol.cizmarik@hotmail.sk>
2023-02-21 22:55:32 +0100

    added express-rate-limit

--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,13 @@
 require('dotenv').config();
+
+// set up rate limiter: maximum of five requests per minute
+const RateLimit = require('express-rate-limit');
+
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 100,
+});
+
 const app = require('./express')();
 // eslint-disable-next-line import/order
 const http = require('http').Server(app);
@@ -9,6 +18,8 @@ const SonosNetwork = require('./sonos/SonosNetwork');
 const io = socketio.listen(http);
 const sonosNetwork = new SonosNetwork(io);
 const routes = new Routes(sonosNetwork);
+
+app.use(limiter);
 
 // Connect all our routes to our app
 app.use('/', routes.router);
